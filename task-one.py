bd8b653dab572ed16367c3c3784581391e6b2483
Veektech001 <ezev2682@gmail.com>
2025-09-05 15:16:00 -0700

    first commit

---
+++ task-one.py
@@ -0,0 +1,16 @@
+x = float(input("Input first number: "))
+z = input()
+y = float(input("Input second value: "))
+
+if z == "*":
+    print(x * y)
+elif z == "/":
+    print(x / y)
+elif z == "+":
+    print(x + y)
+else:
+    print(x + y)
+
+print("Thank you for using my calculator")
+
+
